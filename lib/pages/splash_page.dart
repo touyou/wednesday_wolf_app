9d5a48b67cbcb83529f3150c1b383177eda897a7
touyou <fujiyou1121@gmail.com>
2020-02-19 18:22:59 +0900

    add splash feature

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -9,20 +11,51 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   final _auth = FirebaseAuth.instance;
+  double _logoOpacity = 1;
 
   @override
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 3))
-        .then((value) => _handleTimeout());
+    final timer = Timer.periodic(const Duration(milliseconds: 700), (value) {
+      setState(() {
+        _logoOpacity = _logoOpacity == 1 ? 0.3 : 1.0;
+      });
+    });
+
+    Future.delayed(const Duration(seconds: 3)).then((value) {
+      timer.cancel();
+      _handleTimeout();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 43, 79, 131),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 128),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Image.asset('images/splash_title.png'),
+              const Spacer(flex: 1),
+              AnimatedOpacity(
+                curve: Curves.easeInOut,
+                duration: const Duration(milliseconds: 700),
+                opacity: _logoOpacity,
+                child: Image.asset(
+                  'images/ookami-icon.png',
+                  width: 200,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Image.asset('images/splash_loading.png'),
+              const Spacer(flex: 2),
+              Image.asset('images/splash_comment.png'),
+            ],
+          ),
+        ),
       ),
     );
   }
